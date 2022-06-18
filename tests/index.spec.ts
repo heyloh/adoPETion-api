dff98cd8386ada8c0ac6789d55ca4832e2a4f286
Lohana Torres <lt.leopoldo@gmail.com>
2022-06-17 23:23:10 -0300

    feat: remove unused files

--- tests/index.spec.ts
+++
@@ -1,7 +0,0 @@
-import { sum } from '../src/index';
-describe("Test index", () => {
-  it("should be able to sum two numbers correctly", () => {
-    const result = sum(1, 2);
-    expect(result).toBe(3);
-  })
-})
